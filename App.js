b59ab0d4713baaec86defd14446cdb376c5d3bf3
Gary Tou <garytou2@gmail.com>
2020-07-06 16:03:11 -0700

    Updated app name

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+	  <Text>This will be the FBLA App (tenative name)</Text>
       <StatusBar style="auto" />
     </View>
   );
