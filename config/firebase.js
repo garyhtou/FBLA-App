a18eb3e6039f796b656119eee7b6a3de4b7e8cca
georgezhang02 <georgezhang02@hotmail.com>
2020-07-14 15:40:07 -0700

    Moved firebase setup to config

---
+++ config/firebase.js
@@ -0,0 +1,23 @@
+/**
+ * Firebase configuration and module setups
+ */
+
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYezXvKXApGuyXVij_dB7oXhz8ja3b7FU",
+    authDomain: "mobile-app-development-15940.firebaseapp.com",
+    databaseURL: "https://mobile-app-development-15940.firebaseio.com",
+    projectId: "mobile-app-development-15940",
+    storageBucket: "mobile-app-development-15940.appspot.com",
+    messagingSenderId: "200356083068",
+    appId: "1:200356083068:web:d623a36471a0e0ca39e0f9",
+    measurementId: "G-NS6FVH7163"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var auth = firebase.auth();
+var fs = firebase.firestore();
+
+export default firebase;
