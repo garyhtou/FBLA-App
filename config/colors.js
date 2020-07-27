fad928cd74a429cc52b0775ad823c5a471dd9ecb
Karthik <Shaji>
2020-07-27 15:50:38 -0700

    Changed colors to match color theme

--- config/colors.js
+++ config/colors.js
@@ -7,11 +7,15 @@ import styles from "./styles";
 const colors = {
 
     // Text colors
-    accent: "#E9446A",
+    complement: "#BE2C37",
+    complementAccent: "#cb565e",
+    /*accent: "#E9446A",
     lightText: "#8A8F9E",
-    mediumText: "#414959",
-    white: "#FFFFFF"
-
+    mediumText: "#414959",*/
+    white: "#FFFFFF",
+    mainColor: "#00539D",
+    mainAccent: "#b2cbe1",
+    mainDarkAccent: "#3725b0"
 }
 
 export default colors;
