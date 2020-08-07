a1ddd8b6ea1c4c106febde3f27c006f14b31299d
Gary Tou <garytou2@gmail.com>
2020-08-07 02:28:50 -0700

    Fully moved Sign In/Up screens to Native Base

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -4,185 +4,182 @@
 
 import React from "react";
 import {
-   View,
-   TextInput,
-   TouchableOpacity,
-   StyleSheet,
-   TouchableWithoutFeedback,
-   Keyboard,
-   ActivityIndicator
+	TouchableOpacity,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	Keyboard,
 } from "react-native";
-import { Text } from "native-base";
+import {
+	Text,
+	Container,
+	Footer,
+	View,
+	Content,
+	Spinner,
+	Form,
+	Input,
+	Item,
+	Label,
+	Button,
+} from "native-base";
 import { colors, strings } from "../config/styles";
 import firebase from "../config/firebase";
 
 export default class SignInScreen extends React.Component {
-   state = {
-      email: "",
-      password: "",
-      errorMessage: null,
-      loading: false
-   };
+	state = {
+		email: "",
+		password: "",
+		errorMessage: null,
+		loading: false,
+	};
 
-   handleSignIn = () => {
-      this.setState({errorMessage: null, loading: true})
-      const { email, password } = this.state;
-      return firebase
-         .auth()
-         .signInWithEmailAndPassword(email, password)
-         .then( () => {
-            this.setState({loading: false});
-            this.props.navigation.navigate("App");
-         })
-         .catch((error) => this.setState({ loading: false, errorMessage: error.message }));
-   };
+	handleSignInWithEmail = () => {
+		this.setState({ errorMessage: null, loading: true });
+		const { email, password } = this.state;
+		return firebase
+			.auth()
+			.signInWithEmailAndPassword(email, password)
+			.then(() => {
+				this.setState({ loading: false });
+				this.props.navigation.navigate("App");
+			})
+			.catch((error) =>
+				this.setState({ loading: false, errorMessage: error.message })
+			);
+	};
 
-   render() {
-      return (
-         <TouchableWithoutFeedback
-            onPress={Keyboard.dismiss}
-            accessible={false}
-         >
-            <View style={styles.flexBox}>
-               <Text style={styles.heading}>
-                  Welcome to {strings.appName}
-               </Text>
+	handleSignInWithGoogle = async () => {
+		//can't find anything that works
+	};
 
-               <View style={styles.errorContainer}>
-                  {this.state.errorMessage && (
-                     <Text style={styles.errorText}>
-                        {this.state.errorMessage}
-                     </Text>
-                  )}
-               </View>
+	render() {
+		if (true) {
+			return (
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+					<Container>
+						<Content contentContainerStyle={styles.content}>
+							<Text style={styles.heading}>Welcome to {strings.appName}</Text>
 
-               <View style={styles.form}>
-                  <View>
-                     {/* <Text style={styles.authLabelText}>Email Address</Text> */}
-                     <TextInput
-                        style={styles.authInput}
-                        autoCapitalize="none"
-                        onChangeText={(email) => this.setState({ email })}
-                        value={this.state.email}
-                        placeholder="Email Address"
-                     />
-                  </View>
+							<View style={styles.errorContainer}>
+								{this.state.errorMessage && (
+									<Text style={styles.errorText}>
+										{this.state.errorMessage}
+									</Text>
+								)}
+							</View>
 
-                  <View>
-                     {/* <Text style={styles.authLabelText}>Password</Text> */}
-                     <TextInput
-                        style={styles.authInput}
-                        secureTextEntry
-                        autoCapitalize="none"
-                        onChangeText={(password) => this.setState({ password })}
-                        value={this.state.password}
-                        placeholder="Password"
-                     />
-                  </View>
-               </View>
-
-               <TouchableOpacity
-                  style={styles.authButton}
-                  onPress={this.handleSignIn}
-               >
-                  {this.state.loading ?
-                      (<ActivityIndicator
-                        size="large"
-                        color={colors.white}/>)
-                      :
-                      <Text style={styles.authButtonText}>Sign In</Text>
-                  }
-               </TouchableOpacity>
-
-               <TouchableOpacity
-                  style={styles.footer}
-                  onPress={() => this.props.navigation.navigate("SignUp")}>
-
-                  <Text style={styles.redirectText}>
-                     New to {strings.appName}?{" "}
-                     <Text style={{ color: colors.complementAccent }}>Sign Up</Text>
-                  </Text>
-
-               </TouchableOpacity>
-            </View>
-         </TouchableWithoutFeedback>
-      );
-   }
+							<Form style={styles.form}>
+								<Item floatingLabel>
+									<Label style={styles.authLabelText}>Email Address</Label>
+									<Input
+										style={styles.authInput}
+										autoCapitalize="none"
+										onChangeText={(email) => this.setState({ email })}
+										value={this.state.email}
+									/>
+								</Item>
+								<Item floatingLabel>
+									<Label style={styles.authLabelText}>Password</Label>
+									<Input
+										style={styles.authInput}
+										secureTextEntry
+										autoCapitalize="none"
+										onChangeText={(password) => this.setState({ password })}
+										value={this.state.password}
+									/>
+								</Item>
+								<Button
+									block
+									style={styles.authButton}
+									onPress={this.handleSignInWithEmail}
+								>
+									{this.state.loading ? (
+										<Spinner color={colors.white} />
+									) : (
+										<Text style={styles.authButtonText}>Sign In</Text>
+									)}
+								</Button>
+								{/* <Button full style={styles.authButton}>
+									{this.state.loading ? (
+										<Spinner color="white" />
+									) : (
+										<TouchableOpacity onPress={this.handleSignInWithGoogle}>
+											<Text style={styles.authButtonText}>
+												Sign In with Google
+											</Text>
+										</TouchableOpacity>
+									)}
+								</Button> */}
+							</Form>
+						</Content>
+						<Footer style={styles.footer}>
+							<TouchableOpacity
+								onPress={() => this.props.navigation.navigate("SignUp")}
+							>
+								<Text style={styles.redirectText}>
+									New to {strings.appName}?{" "}
+									<Text style={{ color: colors.complementAccent }}>
+										Sign Up
+									</Text>
+								</Text>
+							</TouchableOpacity>
+						</Footer>
+					</Container>
+				</TouchableWithoutFeedback>
+			);
+		}
+	}
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-   },
-   flexBox: {
-      flex: 1,
-      justifyContent: "center",
-   },
-   heading: {
-      fontSize: 24,
-      textAlign: "center",
-   },
-   errorContainer: {
-      marginTop: 15,
-      alignItems: "center",
-      justifyContent: "center",
-   },
-   errorText: {
-      color: colors.complementAccent,
-      fontSize: 13,
-      textAlign: "center",
-   },
-   form: {
-      marginTop: 15,
-      marginHorizontal: 50,
-   },
-   authLabelText: {
-      color: colors.lightText,
-      fontSize: 11,
-      textTransform: "uppercase",
-   },
-   authButtonText: {
-      color: colors.white,
-      fontSize: 16,
-   },
-   authInput: {
-      borderBottomColor: colors.lightText,
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      height: 40,
-      fontSize: 15,
-      marginBottom: 30,
-   },
-   authButton: {
-      marginHorizontal: 50,
-      backgroundColor: colors.complementAccent,
-      borderRadius: 4,
-      height: 52,
-      alignItems: "center",
-      justifyContent: "center",
-      marginBottom: 50,
-   },
-   signUpHere: {
-      alignSelf: "center",
-      marginTop: 32,
-   },
-   redirectText: {
-      color: colors.mediumText,
-      fontSize: 13,
-   },
-   footer: {
-      position: "absolute",
-      left: 0,
-      right: 0,
-      bottom: 0,
-      alignItems: "center",
-      justifyContent: "center",
-      height: 60,
-      borderTopWidth: 1,
-      borderTopColor: "#d5d5d5",
-   },
-   loading: {
-      color: colors.white,
-   }
+	content: {
+		flex: 1,
+		justifyContent: "center",
+		marginHorizontal: 50,
+	},
+	heading: {
+		fontSize: 24,
+		textAlign: "center",
+		marginBottom: 10,
+	},
+	errorContainer: {
+		marginTop: 15,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	errorText: {
+		color: colors.complementAccent,
+		fontSize: 13,
+		textAlign: "center",
+	},
+	form: {
+		marginTop: 15,
+	},
+	authLabelText: {
+		color: colors.lightText,
+		fontSize: 15,
+		textTransform: "uppercase",
+	},
+	authButtonText: {
+		color: colors.white,
+		fontSize: 16,
+	},
+	authInput: {
+		fontSize: 20,
+	},
+	authButton: {
+		backgroundColor: colors.complementAccent,
+		borderRadius: 4,
+		marginTop: 20,
+	},
+	redirectText: {
+		color: colors.mediumText,
+		fontSize: 13,
+	},
+	footer: {
+		alignItems: "center",
+	},
+	loading: {
+		color: colors.white,
+	},
 });
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -4,198 +4,180 @@
 
 import React from "react";
 import {
-    View,
-    TextInput,
-    TouchableOpacity,
-    StyleSheet,
-    TouchableWithoutFeedback,
-    Keyboard,
-    ActivityIndicator
+	TouchableOpacity,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	Keyboard,
 } from "react-native";
-import { Text } from "native-base";
+import {
+	Text,
+	Container,
+	Footer,
+	View,
+	Content,
+	Spinner,
+	Form,
+	Input,
+	Item,
+	Label,
+	Button,
+} from "native-base";
 import { colors } from "../config/styles";
 import firebase from "../config/firebase";
 
 export default class SignUpScreen extends React.Component {
-   state = {
-      name: "",
-      email: "",
-      password: "",
-      errorMessage: null,
-      loading: false
-   };
+	state = {
+		name: "",
+		email: "",
+		password: "",
+		errorMessage: null,
+		loading: false,
+	};
 
-   handleSignUp = () => {
-      this.setState({errorMessage: null, loading: true});
-      const { name, email, password } = this.state;
-      firebase
-         .auth()
-         .createUserWithEmailAndPassword(email, password)
-         .then((userCredentials) => {
-             this.setState({loading: false});
-             return userCredentials.user.updateProfile({
-                 displayName: name
-             }).then(() => {
-                 this.props.navigation.navigate("App");
-             })
-         })
-         .catch((error) => this.setState({loading: false, errorMessage: error.message }));
-   };
+	handleSignUp = () => {
+		this.setState({ errorMessage: null, loading: true });
+		const { name, email, password } = this.state;
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(email, password)
+			.then((userCredentials) => {
+				this.setState({ loading: false });
+				return userCredentials.user
+					.updateProfile({
+						displayName: name,
+					})
+					.then(() => {
+						this.props.navigation.navigate("App");
+					});
+			})
+			.catch((error) =>
+				this.setState({ loading: false, errorMessage: error.message })
+			);
+	};
 
-   render() {
-      return (
-         <TouchableWithoutFeedback
-            onPress={Keyboard.dismiss}
-            accessible={false}
-         >
-            <View style={styles.flexBox}>
-               <Text style={styles.heading}>Sign up to get started!</Text>
+	render() {
+		return (
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+				<Container>
+					<Content contentContainerStyle={styles.content}>
+						<Text style={styles.heading}>Sign up to get started!</Text>
 
-               <View style={styles.errorContainer}>
-                  {this.state.errorMessage && (
-                     <Text style={styles.errorText}>
-                        {this.state.errorMessage}
-                     </Text>
-                  )}
-               </View>
+						<View style={styles.errorContainer}>
+							{this.state.errorMessage && (
+								<Text style={styles.errorText}>{this.state.errorMessage}</Text>
+							)}
+						</View>
 
-               <View style={styles.form}>
-                  <View>
-                     <TextInput
-                        style={styles.authInput}
-                        autoCapitalize="none"
-                        onChangeText={(name) => this.setState({ name })}
-                        value={this.state.name}
-                        placeholder="Full Name"
-                     />
-                  </View>
+						<Form style={styles.form}>
+							<Item floatingLabel>
+								<Label style={styles.authLabelText}>Full Name</Label>
+								<Input
+									style={styles.authInput}
+									autoCapitalize="none"
+									onChangeText={(name) => this.setState({ name })}
+									value={this.state.name}
+								/>
+							</Item>
 
-                  <View>
-                     <TextInput
-                        style={styles.authInput}
-                        autoCapitalize="none"
-                        onChangeText={(email) => this.setState({ email })}
-                        value={this.state.email}
-                        placeholder="Email"
-                     />
-                  </View>
+							<Item floatingLabel>
+								<Label style={styles.authLabelText}>Email</Label>
+								<Input
+									style={styles.authInput}
+									autoCapitalize="none"
+									onChangeText={(email) => this.setState({ email })}
+									value={this.state.email}
+								/>
+							</Item>
 
-                  <View style={styles.marginTop}>
-                     <TextInput
-                        style={styles.authInput}
-                        secureTextEntry
-                        autoCapitalize="none"
-                        onChangeText={(password) => this.setState({ password })}
-                        value={this.state.password}
-                        placeholder="Password"
-                     />
-                  </View>
-               </View>
+							<Item floatingLabel style={styles.marginTop}>
+								<Label style={styles.authLabelText}>Password</Label>
+								<Input
+									style={styles.authInput}
+									secureTextEntry
+									autoCapitalize="none"
+									onChangeText={(password) => this.setState({ password })}
+									value={this.state.password}
+								/>
+							</Item>
 
-               <TouchableOpacity
-                  style={styles.authButton}
-                  onPress={this.handleSignUp}
-               >
-                  {this.state.loading ?
-                      (<ActivityIndicator
-                          size="large"
-                          color={colors.white}/>)
-                  :
-                      (<Text style={styles.authButtonText}>Sign Up</Text>)
-                  }
-
-               </TouchableOpacity>
-
-               <TouchableOpacity
-                  style={styles.footer}
-                  onPress={() => this.props.navigation.navigate("SignIn")}
-               >
-
-
-                  <Text style={styles.redirectText}>
-                      Already have an account?{" "}
-                      <Text style={{ color: colors.complementAccent }}>
-                          Sign In
-                      </Text>
-                  </Text>
-
-               </TouchableOpacity>
-            </View>
-         </TouchableWithoutFeedback>
-      );
-   }
+							<Button
+								block
+								style={styles.authButton}
+								onPress={this.handleSignUp}
+							>
+								{this.state.loading ? (
+									<Spinner color={colors.white} />
+								) : (
+									<Text style={styles.authButtonText}>Sign Up</Text>
+								)}
+							</Button>
+						</Form>
+					</Content>
+					<Footer style={styles.footer}>
+						<TouchableOpacity
+							onPress={() => this.props.navigation.navigate("SignIn")}
+						>
+							<Text style={styles.redirectText}>
+								Already have an account?{" "}
+								<Text style={{ color: colors.complementAccent }}>Sign In</Text>
+							</Text>
+						</TouchableOpacity>
+					</Footer>
+				</Container>
+			</TouchableWithoutFeedback>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    flexBox: {
-        flex: 1,
-        justifyContent: "center",
-    },
-    heading: {
-        fontSize: 24,
-        textAlign: "center",
-    },
-    errorContainer: {
-        marginTop: 15,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    errorText: {
-        color: colors.complementAccent,
-        fontSize: 13,
-        textAlign: "center",
-    },
-    form: {
-        marginTop: 15,
-        marginHorizontal: 50,
-    },
-    authLabelText: {
-        color: colors.lightText,
-        fontSize: 11,
-        textTransform: "uppercase",
-    },
-    authButtonText: {
-        color: colors.white,
-        fontSize: 16,
-    },
-    authInput: {
-        borderBottomColor: colors.lightText,
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        height: 40,
-        fontSize: 15,
-        marginBottom: 30,
-    },
-    authButton: {
-        marginHorizontal: 50,
-        backgroundColor: colors.complementAccent,
-        borderRadius: 4,
-        height: 52,
-        alignItems: "center",
-        justifyContent: "center",
-        marginBottom: 50,
-    },
-    signUpHere: {
-        alignSelf: "center",
-        marginTop: 32,
-    },
-    redirectText: {
-        color: colors.mediumText,
-        fontSize: 13,
-    },
-    footer: {
-        position: "absolute",
-        left: 0,
-        right: 0,
-        bottom: 0,
-        alignItems: "center",
-        justifyContent: "center",
-        height: 60,
-        borderTopWidth: 1,
-        borderTopColor: "#d5d5d5",
-    },
+	content: {
+		flex: 1,
+		justifyContent: "center",
+		marginHorizontal: 50,
+	},
+	heading: {
+		fontSize: 24,
+		textAlign: "center",
+		marginBottom: 10,
+	},
+	errorContainer: {
+		marginTop: 15,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	errorText: {
+		color: colors.complementAccent,
+		fontSize: 13,
+		textAlign: "center",
+	},
+	form: {
+		marginTop: 15,
+	},
+	authLabelText: {
+		color: colors.lightText,
+		fontSize: 15,
+		textTransform: "uppercase",
+	},
+	authButtonText: {
+		color: colors.white,
+		fontSize: 16,
+	},
+	authInput: {
+		fontSize: 20,
+	},
+	authButton: {
+		backgroundColor: colors.complementAccent,
+		borderRadius: 4,
+		marginTop: 20,
+	},
+	redirectText: {
+		color: colors.mediumText,
+		fontSize: 13,
+	},
+	footer: {
+		alignItems: "center",
+	},
+	loading: {
+		color: colors.white,
+	},
 });
