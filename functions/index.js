4b5c45f81a25fb9b6c9b1b874877ae87054129dc
georgezhang02 <georgezhang02@hotmail.com>
2020-08-16 18:40:17 -0700

    Cloud function to generate chapter code

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.generateCode = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('Codes').doc('Codes').get().then((DocumentSnapshot) => {
+        let dictionary = DocumentSnapshot.data().Codes;
+        let keys = Object.keys(dictionary);
+
+        let index = Math.floor(Math.random()*90000 + 10000);
+        let code = dictionary[index];
+
+        dictionary.delete(keys);
+
+        admin.firestore().collection('Codes').doc('Codes').set({
+            Codes: dictionary
+        }).then(() => {
+            response.send(code);
+            return code;
+        }).catch(error => {
+            response.status(500).send(error);
+        })
+
+        return code;
+
+    })
+    .catch(error => {
+        response.status(500).send(error);
+    })
+});
