4e56633b1d68af099a9333108a018aea09378617
georgezhang02 <georgezhang02@hotmail.com>
2020-08-23 22:32:17 -0700

    Icons for navigation

--- App.js
+++ App.js
@@ -2,13 +2,6 @@
  * App navigation
  */
 
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import LoadingScreen from "./screens/LoadingScreen";
-import SignInScreen from "./screens/SignInScreen";
-import SignUpScreen from "./screens/SignUpScreen";
-import JoinChapScreen from "./screens/JoinChapScreen";
-import CreateChapScreen from "./screens/CreateChapScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AnnouncementScreen from "./screens/AnnouncementScreen";
 import ChapterScreen from "./screens/ChapterScreen";
@@ -16,17 +9,44 @@ import OppScreen from "./screens/ChapterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { Icon } from "native-base";
+import {createAppContainer, createSwitchNavigator} from "react-navigation";
+import JoinChapScreen from "./screens/CreateChapScreen";
+import CreateChapScreen from "./screens/CreateChapScreen";
 import ChapterCodeScreen from "./screens/ChapterCodeScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import LoadingScreen from "./screens/LoadingScreen";
 
 const Tab = createBottomTabNavigator();
 
 function appTabs() {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: () => {
+						let iconName;
+
+						if (route.name === 'Announcements') {
+							iconName = 'home';
+						} else if (route.name === 'Chapter') {
+							iconName = 'work'
+						} else if (route.name === 'Opportunity') {
+							iconName = 'search'
+						} else {
+							iconName = 'person'
+						}
+
+						// You can return any component that you like here!
+						return <Icon name={iconName} type={"MaterialIcons"}/>;
+					},
+				})}
+
+			>
 				<Tab.Screen name="Announcements" component={AnnouncementScreen} />
 				<Tab.Screen name="Chapter" component={ChapterScreen} />
-				<Tab.Screen name="Opportuninty" component={OppScreen} />
+				<Tab.Screen name="Opportunity" component={OppScreen} />
 				<Tab.Screen name="Profile" component={ProfileScreen} />
 			</Tab.Navigator>
 		</NavigationContainer>
